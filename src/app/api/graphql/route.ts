2371a4ca854b17d7e335fea9264f0c5e50725ea9
imevanc <charal.vag@gmail.com>
2024-02-10 16:01:18 +0000

    delete source code of apollo server

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,27 +1,2 @@
-import type { NextRequest } from "next/server";
-import { startServerAndCreateNextHandler } from "@as-integrations/next";
-import { ApolloServer } from "@apollo/server";
-import { gql } from "graphql-tag";
-
-const resolvers = {
-  Query: {
-    hello: () => "world",
-  },
-};
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-const server = new ApolloServer({
-  resolvers,
-  typeDefs,
-});
-
-const handler = startServerAndCreateNextHandler<NextRequest>(server, {
-  context: async (req) => ({ req }),
-});
-
+const handler = "";
 export { handler as GET, handler as POST };
