a5a1e665c551229c84647e175bb292e12f0573c5
imevanc <charal.vag@gmail.com>
2024-07-06 22:45:55 +0100

    fix playwright config issue

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,16 +1,18 @@
-import {expect, test} from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+test("has title", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
 
   await expect(page).toHaveTitle(/Next.js starter/);
 });
 
-test('has all the accessible elements', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+test("has all the accessible elements", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
 
-  await expect(page.getByRole('heading', { name: /Welcome to the Next.js starter/i})).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: /Welcome to the Next.js starter/i }),
+  ).toBeVisible();
 
-  await expect(page.getByRole('button', { name: /Button 1/ })).toBeVisible();
-  await expect(page.getByRole('button', { name: /Button 2/ })).toBeVisible();
+  await expect(page.getByRole("button", { name: /Button 1/ })).toBeVisible();
+  await expect(page.getByRole("button", { name: /Button 2/ })).toBeVisible();
 });
