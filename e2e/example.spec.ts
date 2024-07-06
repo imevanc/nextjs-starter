014296018df1355b2c7013eb65c4bb85186febb4
imevanc <charal.vag@gmail.com>
2024-07-06 22:54:02 +0100

    update domain

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,13 +1,13 @@
-import { expect, test } from "@playwright/test";
+import {expect, test} from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto("https://nextjs-starter-delta-one.vercel.app/");
 
   await expect(page).toHaveTitle(/Next.js starter/);
 });
 
 test("has all the accessible elements", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto("https://nextjs-starter-delta-one.vercel.app/");
 
   await expect(
     page.getByRole("heading", { name: /Welcome to the Next.js starter/i }),
