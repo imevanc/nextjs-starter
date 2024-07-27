86ab0054a0ccdd85d3f4b13ab0478a60a7f82e86
imevanc <charal.vag@gmail.com>
2024-07-27 12:14:37 +0100

    fix: update ci

--- test/pages/Home.spec.tsx
+++ test/pages/Home.spec.tsx
@@ -1,41 +1,29 @@
-import {screen} from "@testing-library/react";
-import {expect, test} from "@jest/globals";
-import {renderPage} from "../utils/renderPage";
+import { screen } from "@testing-library/react";
+import { expect, test } from "@jest/globals";
+import { renderPage } from "../utils/renderPage";
 import Home from "@/app/page";
 
 test("render the Homepage", async () => {
   renderPage(<Home />);
   expect(
-      screen.getByRole("heading", { name: /Unleashing Creativity: Powered by Generative AI/i }),
+    screen.getByRole("heading", {
+      name: /Unleashing Creativity: Powered by Generative AI/i,
+    }),
   ).toBeVisible();
   expect(
-      screen.getByText('Announcing our next round of funding.')
+    screen.getByText("Announcing our next round of funding."),
   ).toBeVisible();
   expect(
-      screen.getByText('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend tempor nisi, eget suscipit ante elementum ac. Praesent euismod arcu lacus, sit amet mollis libero egestas vel.')
+    screen.getByText(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend tempor nisi, eget suscipit ante elementum ac. Praesent euismod arcu lacus, sit amet mollis libero egestas vel.",
+    ),
   ).toBeVisible();
-  expect(
-      screen.getByRole('link', {name:/read more/i})
-  ).toBeVisible();
-  expect(
-      screen.getByRole('link', {name:/get started/i})
-  ).toBeVisible();
-  expect(
-      screen.getByRole('link', {name:/learn more/i})
-  ).toBeVisible();
-  expect(
-      screen.getByRole('link', {name:/log in/i})
-  ).toBeVisible();
-  expect(
-      screen.getByTestId('Product-desktop')
-  ).toBeVisible();
-  expect(
-      screen.getByTestId('Features-desktop')
-  ).toBeVisible();
-  expect(
-      screen.getByTestId('Marketplace-desktop')
-  ).toBeVisible();
-  expect(
-      screen.getByTestId('Company-desktop')
-  ).toBeVisible();
+  expect(screen.getByRole("link", { name: /read more/i })).toBeVisible();
+  expect(screen.getByRole("link", { name: /get started/i })).toBeVisible();
+  expect(screen.getByRole("link", { name: /learn more/i })).toBeVisible();
+  expect(screen.getByRole("link", { name: /log in/i })).toBeVisible();
+  expect(screen.getByTestId("Product-desktop")).toBeVisible();
+  expect(screen.getByTestId("Features-desktop")).toBeVisible();
+  expect(screen.getByTestId("Marketplace-desktop")).toBeVisible();
+  expect(screen.getByTestId("Company-desktop")).toBeVisible();
 });
