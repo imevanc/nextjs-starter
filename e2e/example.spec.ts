fb5614d202a31c6f8e440064e3670f8c318bedc6
Evan Charalampidis <96417438+imevanc@users.noreply.github.com>
2024-07-27 12:07:20 +0100

    feat: create new homepage (#56)

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "@playwright/test";
+import {expect, test} from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto("https://nextjs-starter-delta-one.vercel.app/");
@@ -10,9 +10,32 @@ test("has all the accessible elements", async ({ page }) => {
   await page.goto("https://nextjs-starter-delta-one.vercel.app/");
 
   await expect(
-    page.getByRole("heading", { name: /Welcome to the Next.js starter/i }),
+    page.getByRole("heading", { name: /Unleashing Creativity: Powered by Generative AI/i }),
   ).toBeVisible();
-
-  await expect(page.getByRole("button", { name: /Button 1/ })).toBeVisible();
-  await expect(page.getByRole("button", { name: /Button 2/ })).toBeVisible();
+  await expect(
+      page.getByText('Announcing our next round of funding.')
+  ).toBeVisible();
+  await expect(
+      page.getByText('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend tempor nisi, eget suscipit ante elementum ac. Praesent euismod arcu lacus, sit amet mollis libero egestas vel.')
+  ).toBeVisible();
+  await expect(
+      page.getByRole('link', {name:/read more/i})
+  ).toBeVisible();
+  await expect(
+      page.getByRole('link', {name:/get started/i})
+  ).toBeVisible();
+  await expect(
+      page.getByRole('link', {name:/learn more/i})
+  ).toBeVisible();
+  await expect(
+      page.getByRole('link', {name:/log in/i})
+  ).toBeVisible();await expect(
+      page.getByTestId('Product-desktop')
+  ).toBeVisible();await expect(
+      page.getByTestId('Features-desktop')
+  ).toBeVisible();await expect(
+      page.getByTestId('Marketplace-desktop')
+  ).toBeVisible();await expect(
+      page.getByTestId('Company-desktop')
+  ).toBeVisible();
 });
