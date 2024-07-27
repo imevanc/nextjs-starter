20af963a7e7d0ed076c7feb95bd860d7cab02828
imevanc <charal.vag@gmail.com>
2024-07-27 12:01:32 +0100

    feat: create new homepage and update playwright

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "@playwright/test";
+import {expect, test} from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto("https://nextjs-starter-delta-one.vercel.app/");
@@ -7,12 +7,35 @@ test("has title", async ({ page }) => {
 });
 
 test("has all the accessible elements", async ({ page }) => {
-  await page.goto("https://nextjs-starter-delta-one.vercel.app/");
+  await page.goto("http://localhost:3000/");
 
   await expect(
-    page.getByRole("heading", { name: /Welcome to the Next.js starter/i }),
+    page.getByRole("heading", { name: /Unleashing Creativity: Powered by Generative AI/i }),
   ).toBeVisible();
-
-  await expect(page.getByRole("button", { name: /Button 1/ })).toBeVisible();
-  await expect(page.getByRole("button", { name: /Button 2/ })).toBeVisible();
+  await expect(
+      page.getByText('Announcing our next round of funding.')
+  ).toBeVisible();
+  await expect(
+      page.getByText('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend tempor nisi, eget suscipit ante elementum ac. Praesent euismod arcu lacus, sit amet mollis libero egestas vel.')
+  ).toBeVisible();
+  await expect(
+      page.getByRole('link', {name:/read more/i})
+  ).toBeVisible();
+  await expect(
+      page.getByRole('link', {name:/get started/i})
+  ).toBeVisible();
+  await expect(
+      page.getByRole('link', {name:/learn more/i})
+  ).toBeVisible();
+  await expect(
+      page.getByRole('link', {name:/log in/i})
+  ).toBeVisible();await expect(
+      page.getByTestId('Product-desktop')
+  ).toBeVisible();await expect(
+      page.getByTestId('Features-desktop')
+  ).toBeVisible();await expect(
+      page.getByTestId('Marketplace-desktop')
+  ).toBeVisible();await expect(
+      page.getByTestId('Company-desktop')
+  ).toBeVisible();
 });
