1fd39fa290bc76e1273c799e351b3dd8108953a4
imevanc <charal.vag@gmail.com>
2025-08-26 13:43:05 +0100

    fix: update renderPage function with overrides

--- test/utils/renderPage.tsx
+++ test/utils/renderPage.tsx
@@ -3,5 +3,8 @@ import { render } from "@testing-library/react";
 
 type Overrides = {};
 
-export const renderPage = (children: ReactNode, overrides?: Overrides) =>
+export const renderPage = (children: ReactNode, overrides?: Overrides) => {
+  // You can update the componet props with the overrides here
+  console.log("overrides", overrides);
   render(children);
+};
